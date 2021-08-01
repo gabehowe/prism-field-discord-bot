9a819a487dc5b65f3e8fcd17f75fcb094aac3492
MIdnightfury <31964173+MIdnightfury@users.noreply.github.com>
2021-08-01 18:23:31 -0400

    remove bot token

--- commands/ping.py
+++
@@ -1,6 +0,0 @@
-from classes.interaction import InteractionResponse, Interaction
-from classes.slashcommandmanager import *
-
-
-async def on_command(interaction: Interaction):
-    await interaction.reply_text('pong', True)
